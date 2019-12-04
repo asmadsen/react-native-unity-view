9e4f1b68099371f56f2704c5a0a10eeaa450b961
Andreas Storesund Madsen <andreas@asmadsen.no>
2019-12-04 15:22:40 +0100

    Update Build.cs

--- template/Build.cs
+++ template/Build.cs
@@ -61,7 +61,7 @@ public class Build : MonoBehaviour {
         build_text = build_text.Replace("bundle {", "splits {");
         build_text = build_text.Replace("enableSplit = false", "enable false");
         build_text = build_text.Replace("enableSplit = true", "enable true");
-        build_text = build_text.Replace("implementation fileTree(dir: 'libs', include: ['*.jar'])", "implementation project(':unity-classes')");
+        build_text = build_text.Replace("implementation fileTree(dir: 'libs', include: ['*.jar'])", "implementation ':unity-classes'");
         build_text = Regex.Replace(build_text, @"\n.*applicationId '.+'.*\n", "\n");
         File.WriteAllText(build_file, build_text);
 
